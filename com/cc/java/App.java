2807a877c5ab62b2a112787c45bc1de6cb63a1f6
bacaiib <roman-frommhold@gmx.net>
2024-01-24 10:52:51 +0100

    Instanziierung mit Referenzvariable

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,6 +4,20 @@ public class App {
     
     public static void main(String[] args) {
 
+        // Objekt ensteht..
+        Cat cat = new Cat(); // Instanziierung..
+        System.out.println(cat);
+
+        // cat.sayHall0();
+        cat.tellYourAddress();
+
+        System.out.println("------------------");
+
+        Cat cat2 = new Cat();
+        System.out.println(cat2);
+        cat2.tellYourAddress();
+
+
 
     }
 
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,17 @@
+package com.cc.java;
+
+public class Cat {
+
+    public void sayHall0(){
+       
+        System.out.println("Hello");
+    }
+
+    public void tellYourAddress(){
+
+        System.out.println(this);
+
+    }
+    
+
+}
